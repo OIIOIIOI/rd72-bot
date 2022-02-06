ef57f4c565861585d3c201734090f1a7b4986817
Sylvere Armange <sylvere.armange@gmail.com>
2022-02-06 20:16:44 +0100

    trainings for feb. 2022

--- data/trainings.js
+++ data/trainings.js
@@ -8,12 +8,12 @@ module.exports = {
 		{
 			date: "2022-02-07 20:00",
 			teams: ["rcp", "fresh"],
-			location: "spot",
+			location: "vauguyon",
 		},
 		{
 			date: "2022-02-07 20:00",
 			teams: ["chica", "missfeet"],
-			location: "vauguyon",
+			location: "spot",
 		},
 		{
 			date: "2022-02-12 10:00",
@@ -22,13 +22,13 @@ module.exports = {
 		},
 		{
 			date: "2022-02-14 20:00",
-			teams: ["rcp", "missfeet"],
-			location: "spot",
+			teams: ["chica", "missfeet"],
+			location: "vauguyon",
 		},
 		{
 			date: "2022-02-14 20:00",
-			teams: ["chica", "fresh"],
-			location: "vauguyon",
+			teams: ["rcp", "fresh"],
+			location: "spot",
 		},
 		{
 			date: "2022-02-19 10:00",
@@ -37,12 +37,12 @@ module.exports = {
 		},
 		{
 			date: "2022-02-21 20:00",
-			teams: ["rcp", "fresh"],
+			teams: ["rcp", "missfeet"],
 			location: "vauguyon",
 		},
 		{
 			date: "2022-02-21 20:00",
-			teams: ["chica", "missfeet"],
+			teams: ["chica", "fresh"],
 			location: "spot",
 		},
 		{
@@ -52,12 +52,12 @@ module.exports = {
 		},
 		{
 			date: "2022-02-28 20:00",
-			teams: ["rcp", "missfeet"],
+			teams: ["rcp", "fresh"],
 			location: "vauguyon",
 		},
 		{
 			date: "2022-02-28 20:00",
-			teams: ["chica", "fresh"],
+			teams: ["chica", "missfeet"],
 			location: "spot",
 		},
 		/*{
