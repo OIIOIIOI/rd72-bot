049ce4a220fe03b8e3c653074fb13daa5da3f45f
Sylvere Armange <sylvere.armange@gmail.com>
2022-03-04 17:57:39 +0100

    trainings for mar. 2022

--- data/trainings.js
+++ data/trainings.js
@@ -1,70 +1,70 @@
 module.exports = {
 	data: [
 		{
-			date: "2022-02-05 10:00",
+			date: "2022-03-05 10:00",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-02-07 20:00",
-			teams: ["rcp", "fresh"],
-			location: "vauguyon",
-		},
-		{
-			date: "2022-02-07 20:00",
-			teams: ["chica", "missfeet"],
+			date: "2022-03-07 20:00",
+			teams: ["rcp", "missfeet"],
 			location: "spot",
 		},
 		{
-			date: "2022-02-12 10:00",
+			date: "2022-03-07 20:00",
+			teams: ["chica", "fresh"],
+			location: "vauguyon",
+		},
+		{
+			date: "2022-03-12 10:00",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-02-14 20:00",
+			date: "2022-03-14 20:00",
 			teams: ["chica", "missfeet"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-02-14 20:00",
+			date: "2022-03-14 20:00",
 			teams: ["rcp", "fresh"],
 			location: "spot",
 		},
 		{
-			date: "2022-02-19 10:00",
+			date: "2022-03-19 10:00",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-02-21 20:00",
+			date: "2022-03-21 20:00",
 			teams: ["rcp", "missfeet"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-02-21 20:00",
+			date: "2022-03-21 20:00",
 			teams: ["chica", "fresh"],
 			location: "spot",
 		},
 		{
-			date: "2022-02-26 10:00",
+			date: "2022-03-26 10:00",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-02-28 20:00",
+			date: "2022-03-28 20:00",
 			teams: ["rcp", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-02-28 20:00",
+			date: "2022-03-28 20:00",
 			teams: ["chica", "missfeet"],
 			location: "spot",
 		},
-		/*{
-			date: "2022-01-26 20:30",
+		{
+			date: "2022-03-30 20:30",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "crossfit",
 			note: "Crossfit !",
-		},*/
+		},
 	]
 }
