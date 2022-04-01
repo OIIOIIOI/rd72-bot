57bc5d33021f1f825696abb2bd54fe8efadddd77
Sylvere Armange <sylvere.armange@gmail.com>
2022-04-01 19:38:34 +0200

    trainings for apr. 2022

--- data/trainings.js
+++ data/trainings.js
@@ -1,70 +1,75 @@
 module.exports = {
 	data: [
 		{
-			date: "2022-03-05 10:00",
+			date: "2022-04-02 10:00",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-03-07 20:00",
+			date: "2022-04-04 20:00",
 			teams: ["rcp", "missfeet"],
 			location: "spot",
 		},
 		{
-			date: "2022-03-07 20:00",
-			teams: ["chica", "fresh"],
+			date: "2022-04-04 20:00",
+			teams: ["fresh", "chica"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-03-12 10:00",
+			date: "2022-04-09 10:00",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-03-14 20:00",
-			teams: ["chica", "missfeet"],
-			location: "vauguyon",
-		},
-		{
-			date: "2022-03-14 20:00",
+			date: "2022-04-11 20:00",
 			teams: ["rcp", "fresh"],
 			location: "spot",
 		},
 		{
-			date: "2022-03-19 10:00",
+			date: "2022-04-11 20:00",
+			teams: ["missfeet", "chica"],
+			location: "vauguyon",
+		},
+		{
+			date: "2022-04-16 10:00",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-03-21 20:00",
+			date: "2022-04-18 20:00",
+			teams: ["chica", "fresh"],
+			location: "spot",
+		},
+		{
+			date: "2022-04-18 20:00",
 			teams: ["rcp", "missfeet"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-03-21 20:00",
-			teams: ["chica", "fresh"],
-			location: "spot",
-		},
-		{
-			date: "2022-03-26 10:00",
+			date: "2022-04-23 10:00",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-03-28 20:00",
+			date: "2022-04-25 20:00",
 			teams: ["rcp", "fresh"],
 			location: "vauguyon",
 		},
 		{
-			date: "2022-03-28 20:00",
+			date: "2022-04-25 20:00",
 			teams: ["chica", "missfeet"],
 			location: "spot",
 		},
 		{
-			date: "2022-03-30 20:30",
+			date: "2022-04-27 20:30",
 			teams: ["missfeet", "rcp", "chica", "fresh"],
 			location: "crossfit",
 			note: "Crossfit !",
+		},
+		{
+			date: "2022-04-30 10:00",
+			teams: ["missfeet", "rcp", "chica", "fresh"],
+			location: "vauguyon",
 		},
 	]
 }
